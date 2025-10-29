00daafdb8d49ac364fc7180b5a2ebe2ee0302102
SILSAIR <silsairsan@gmail.com>
2025-10-29 18:29:57 +0200

    hi

---
+++ app/src/main/java/com/example/myapplication/LoadingActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.loading)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+                val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+        }, 3000) // 3000 is the delayed time in milliseconds
+    }
+}
